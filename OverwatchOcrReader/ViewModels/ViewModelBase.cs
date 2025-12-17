620fc308fd42d7342f809b3ef4908ceb58ef45bf
Kittelchen <github@kittelchen.de>
2025-12-18 00:48:40 +0100

    Add project files.

---
+++ OverwatchOcrReader/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OverwatchOcrReader.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ OverwatchOcrReader/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace OverwatchOcrReader.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
